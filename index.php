38fba33e3282179aaa1cf8192a41aaa171723757
asifulmamun <asifulmamun@gmail.com>
2022-04-28 16:55:08 +0600

    js css and site infor edit from init.php

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
 
-  // Home Page
-  require_once './result/config/config.php'
+    // Home Page
+    require_once './init.php';
+    require_once './result/config/conn.php';
 
+
+    // Call Site Information from init.php
+    $site_info = new Info();
+    
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,9 +16,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <title><?php echo $template_titile; ?></title>
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="shortcut icon" href="<?php echo $site_info->favicon_icon; ?>" type="image/x-icon">
+    <title><?php echo $site_info->template_name; ?></title>
+    <link rel="stylesheet" href="<?php echo $site_info->dir_css; ?>style.css">
 </head>
 
 <body>
@@ -22,13 +27,12 @@
         <div class="content_wrap">
             <div class="sidebar_left">
                 <aside>
-                    <header><a href="./"><img src="./media/logo.png" alt="Logo"></a>
-                        <h1><?php echo $site_title; ?></h1>
+                    <header><a href="./"><img src="<?php echo $site_info->logo; ?>" alt="Logo"></a>
+                        <h1><?php echo $site_info->site_title; ?></h1>
                     </header>
                     <nav>
                         <ul>
-                            <li class="side_menu_li side_menu_li_1"><a class="side_menu_li_a_1"
-                                    href="./">ফলাফল</a></li>
+                            <li class="side_menu_li side_menu_li_1"><a class="side_menu_li_a_1" href="./">ফলাফল</a></li>
                         </ul>
                     </nav>
                 </aside>
@@ -41,7 +45,7 @@
         </div>
         <div id="route2"></div>
     </section>
-    <script src="./js/route.js"></script>
+    <script src="<?php echo $site_info->dir_js; ?>route.js"></script>
     <div id="route"></div>
     <small>Result By: <a href="https://asifulmamun.info">www.asifulmamun.info</a></small>
 </body>
